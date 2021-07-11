4f56fe3d5c1f7c680d986540395f4c76b50457ef
PatriciaAndrews <86771758+PatriciaAndrews@users.noreply.github.com>
2021-07-11 12:24:54 +0400

    for MySQL

--- restful-web-services/src/main/java/com/sample/rest/webservices/restfulwebservices/todo/Todo.java
+++ restful-web-services/src/main/java/com/sample/rest/webservices/restfulwebservices/todo/Todo.java
@@ -5,12 +5,13 @@ import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Todo {
 	@Id
-	@GeneratedValue 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String username;
 	private String description;
